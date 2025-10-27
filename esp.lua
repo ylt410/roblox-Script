7b4d3ff5b14e77476ef26113c6bbaf9bfdc7beb6
ylt410 <wsylt120420@outlook.com>
2025-10-27 20:14:59 +0800

    更新 esp.lua

--- esp.lua
+++ esp.lua
@@ -1,3 +1,3 @@
 -- 放在 LocalScript（StarterPlayerScripts）
 local player = game.Players.LocalPlayer
-player:Kick("此脚本已更新，请在注入链接的esp后面")  -- 立即断开并显示消息
+player:Kick("此脚本已更新，请把注入链接的esp改为esp2重新注入，谢谢")  -- 立即断开并显示消息
